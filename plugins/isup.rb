efcd2ded9e10e258f5823994120b7f38d6e0d4b3
Mario de la Ossa <mariodelaossa@gmail.com>
2013-09-17 23:18:03 -0400

    Added IsUp module

---
+++ plugins/isup.rb
@@ -0,0 +1,28 @@
+# encoding: utf-8
+
+class IsUp
+    include Cinch::Plugin
+
+	match /isup (.+)/i, method: :isup
+
+	def isup(m, word)
+		return unless ignore_nick(m.user.nick).nil?
+
+		begin
+			@bitly = Bitly.new($BITLYUSER, $BITLYAPI)
+
+			number ||= 1
+
+			url = CGI.escape(word)
+			#isup = Nokogiri::HTML(open(url))
+			resp = Net::HTTP.get_response(URI.parse(url))
+			
+			if resp.code.match('200')
+			    m.reply "IsUp | #{word} | Seems up to me! Sucks to be you."
+			else
+			    m.reply "IsUp | #{word} | Seems down to me"
+		rescue
+			m.reply "IsUp | #{word} | Error occurred"
+		end
+	end
+end
