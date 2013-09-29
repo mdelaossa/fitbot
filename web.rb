9579ba85a11e8af360d5f089af3f1a9c188d9a6b
Mario de la Ossa <mariodelaossa@gmail.com>
2013-09-28 22:20:59 -0400

    Added json routes

--- web.rb
+++ web.rb
@@ -10,6 +10,14 @@ Thread.new {
             erb :index 
         end
         
+        get '/:name.json' do |name|
+            Nick.first(:nick => name.downcase).to_json(:only => [:nick, :weight, :height])
+        end
+        
+        get '/lifts/:name.json' do |name|
+            Nick.first(:nick => name.downcase).lifts.to_json(:only => [:lift, :weight, :unit, :reps])
+        end
+        
         get '/lifts/?:name?' do |name|
             erb :lifts, :locals => {:name => name}
         end
