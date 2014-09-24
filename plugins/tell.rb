06f0451e4387253da03b70910e1a9bba1c7cb4e9
Mario de la Ossa <mariodelaossa@gmail.com>
2014-09-24 22:55:41 +0000

    Tell: added .inbox. Message confirmations on PM. Remaining messages count

--- plugins/tell.rb
+++ plugins/tell.rb
@@ -20,32 +20,49 @@ class Tell
   
   listen_to :channel
   
-  match /tell\s+(\w+)\s+(.+$)/i
+  match /tell\s+(\S+)\s+(.+$)/i, method: :execute
+  match /inbox$/i, method: :pmAllMessages
   
   def execute(m, recipient, message)
     begin
-      outbox = Messages.create(:sender => m.user.nick, :recipient => recipient, :sent_at => Time.now, :text => message, :channel => m.channel, :network => @bot.irc.network.name)
+      outbox = Messages.create(:sender => m.user.nick.downcase, :recipient => recipient.downcase, :sent_at => Time.now, :text => message, :channel => m.channel, :network => @bot.irc.network.name)
       if outbox.nil?
+        error "Error creating message:"
         error outbox
-        m.reply "Tell | Error creating message", true
+        m.user.send "Tell | Error creating message"
       else
         debug "created Tell message:"
         debug outbox.inspect
-        m.reply "Tell | Message created successfully", true
+        m.user.send "Tell | Message created successfully: user=#{recipient}, message=#{message}"
       end
     rescue => x
       error x.message
       error x.backtrace.inspect
-			m.reply "Tell | Error creating message"
+			m.user.send "Tell | Error creating message"
     end
   end
   
   def listen(m)
-    message = Messages.first :recipient => m.user.nick, :channel => m.channel, :network => @bot.irc.network.name
-    if !message.nil?
-      formatted_message = "✉ | #{message.text} · from #{message.sender} · #{relative_time(message.sent_at.to_time)}"
+    unless m.message =~ /^.inbox$/ #don't show a message in channel if 
+      messages = Messages.all :recipient => m.user.nick.downcase, :channel => m.channel, :network => @bot.irc.network.name
+      if !messages.empty?
+        formatted_message = "✉ | #{messages[0].text} · from #{messages[0].sender} · #{relative_time(messages[0].sent_at.to_time)}"
+        formatted_message += " · Reimaining: #{messages.size - 1}" if messages.size > 1
+        messages[0].destroy
+        m.reply(formatted_message, true)
+      end
+    end
+  end
+  
+  def pmAllMessages(m)
+    messages = Messages.all :recipient => m.user.nick.downcase, :network => @bot.irc.network.name
+    header = "✉ | Messages: #{messages.size}"
+    header += " · showing first 5 only" if messages.size > 5
+    m.user.send header
+    messages[0..4].each do |message|
+      formatted_message = "✉ | #{message.text} · from #{message.sender} · channel #{message.channel} · #{relative_time(message.sent_at.to_time)}"
       message.destroy
-      m.reply(formatted_message, true)
+      m.user.send formatted_message
     end
   end
   
