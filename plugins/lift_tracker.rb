9cf73eff5ca2c14a6c951c1027797277e5a64c18
Mario de la Ossa <mariodelaossa@gmail.com>
2014-09-25 21:27:05 +0000

    LiftTracker: cosmetic, 5'6" instead of 5ft 6in

--- plugins/lift_tracker.rb
+++ plugins/lift_tracker.rb
@@ -127,7 +127,7 @@ class LiftTracker
             "#{(nick.height / 100).round(2)}m"
         else
             height_in_ft = (nick.height / 2.54 / 12)
-            "#{height_in_ft.to_i}ft #{((height_in_ft - height_in_ft.to_i)*12).round(0)}in" #decimals * 12 /10 * 10
+            "#{height_in_ft.to_i}'#{((height_in_ft - height_in_ft.to_i)*12).round(0)}\" #decimals * 12 /10 * 10
         end
     end
     
