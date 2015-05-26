5ca9f7f49ac4fbf7be07cc4709b4fb60833c74c6
Mario de la Ossa <mariodelaossa@gmail.com>
2015-05-26 18:01:17 +0000

    Added votequiet and votekick

--- lib/plugins/voteban.rb
+++ lib/plugins/voteban.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 
-class VoteBan
+class Voting
     include Cinch::Plugin
     include UtilityFunctions
     
@@ -19,7 +19,7 @@ class VoteBan
         @ballots.find { |ballot| ballot.channel == channel}
     end
 
-    match /voteban registered/, method: :registered
+    match /voting registered/, method: :registered
     def registered(m)
         return unless ignore_nick(m.user.nick).nil?
         return unless check_admin_kick(m)
@@ -32,8 +32,7 @@ class VoteBan
         end
     end
 
-    match /voteban cancel/i, method: :cancel
-    match /vb cancel/i, method: :cancel
+    match /voting cancel/i, method: :cancel
     def cancel(m)
         begin
             return unless ignore_nick(m.user.nick).nil?
@@ -44,29 +43,27 @@ class VoteBan
             @ballots.delete ballot
             ballot.timeout_job.unschedule
 
-            m.reply "VoteBan | Vote on #{ballot.defendant} cancelled"
+            m.reply "Voting | Vote on #{ballot.defendant} cancelled"
         rescue Exception => e
-            m.reply "VoteBan | Error: #{e}"
+            m.reply "Voting | Error: #{e}"
         end
     end
     
-    match /voteban threshold \d+/i, method: :threshold
-    match /vb threshold \d+/i, method: :threshold
+    match /voting threshold \d+/i, method: :threshold
     def threshold(m, num)
         return unless check_admin_kick(m)
         @threshold = num
-        m.reply "VoteBan | Threshold changed to #{num}"
+        m.reply "Voting | Threshold changed to #{num}"
     end
     
-    match /voteban$/i, method: :tally
-    match /vb$/i, method: :tally
+    match /voting$/i, method: :tally
     def tally(m)
         begin
             ballot = find_ballot(m.channel)
             raise 'No vote in progress' if ballot.nil?
-            m.reply "VoteBan | #{ballot.tally}"
+            m.reply "Voting | #{ballot.tally}"
         rescue Exception => e
-            m.reply "VoteBan | Error: #{e}"
+            m.reply "Voting | Error: #{e}"
         end
     end
     
@@ -82,54 +79,82 @@ class VoteBan
             ballot.vote vote, m.user 
             
             if ballot.yes >= @threshold 
-                m.channel.ban(ballot.defendant.mask("*!*@%h"))
-		        m.channel.kick(ballot.defendant, "The people have spoken. 30 minute ban.")
-		        @@timer.in '30m' do
-		            m.channel.unban(ballot.defendant.mask("*!*@%h"))
+                case ballot.type
+                when :ban
+                    m.channel.ban(ballot.defendant.mask("*!*@%h"))
+		            m.channel.kick(ballot.defendant, "The people have spoken. 30 minute ban.")
+		            @@timer.in '30m' do
+		                m.channel.unban(ballot.defendant.mask("*!*@%h"))
+		            end
+		        when :kick
+		            m.channel.kick(ballot.defendant, "Buh bye")
+		        when :quiet
+		            m.channel.mode("+q #{ballot.defendant.mask("*!*@%h")}")
+		            m.channel.send("Quieted for 30 minutes")
+		            @@timer.in '30m' do
+		                m.channel.mode("-q #{ballot.defendant.mask("*!*@%h")}")
+		            end
 		        end
+		        
 		        @ballots.delete ballot ##end ballot
 		        ballot.timeout_job.unschedule
-		        m.reply "VoteBan | Another win for democracy!"
+		        m.reply "Voting | Another win for democracy!"
 		        return
             end
             
             if ballot.no >= @threshold
                 @ballots.delete ballot ##end ballot
                 ballot.timeout_job.unschedule
-		        m.reply "VoteBan | Vote failed. #{ballot.defendant} got lucky this time." 
+		        m.reply "Voting | Vote failed. #{ballot.defendant} got lucky this time." 
 		        return
             end
             
-            m.reply "VoteBan | Vote added. #{ballot.tally}"
+            m.reply "Voting | Vote added. #{ballot.tally}"
             
 	    rescue Exception => e
-	        m.reply "VoteBan | Error occured: #{e}", true
+	        m.reply "Voting | Error occured: #{e}", true
 	    end
 	end
 
-	match /voteban (?!(?:registered|cancel|threshold \d+)$)(.+)/i, method: :voteban
-	match /vb (?!(?:registered|cancel|threshold \d+)$)(.+)/i, method: :voteban
+	match /voteban (.+)/i, method: :voteban
+	match /vb (.+)/i, method: :voteban
 	def voteban(m, defendant)
-		return unless ignore_nick(m.user.nick).nil?
+		start_vote(m, defendant, :ban)
+	end
+	
+	match /votekick (.+)/i, method: :votekick
+	match /vk (.+)/i, method: :votekick
+	def votekick(m, defendant)
+	    start_vote(m, defendant, :kick)
+	end
+	
+	match /votequiet (.+)/i, method: :votekick
+	match /vq (.+)/i, method: :votekick
+	def votequiet(m, defendant)
+	    start_vote(m, defendant, :quiet)
+	end
+	
+	def start_vote(m, defendant, type)
+	    return unless ignore_nick(m.user.nick).nil?
 		begin
 		    defendant = User(defendant)
 		    
-		    raise 'Vote already in progress' unless find_ballot(m.channel).nil?
-		    raise "User not online" if defendant.nil?
-		    raise "User not online" if defendant.host.nil?
-		    raise "You can't ban that person!" if check_admin(defendant)
-		    raise "Only registered nicks can vote" if m.user.authname.nil? and @registered
-		    
-		    ballot = Ballot.new(defendant, m.user, m.channel)
-		    @ballots << ballot
-		    
-		    ballot.timeout_job = @@timer.schedule_in '5m' do
-		        cancel(m)
-		    end
-		    
-			m.reply "VoteBan | #{defendant} | Vote started! Please vote on this ban with .vote yes|no"
+    	    raise 'Vote already in progress' unless find_ballot(m.channel).nil?
+    	    raise "User not online" if defendant.nil?
+    	    raise "User not online" if defendant.host.nil?
+    	    raise "You can't #{type} that person!" if check_admin(defendant)
+    	    raise "Only registered nicks can vote" if m.user.authname.nil? and @registered
+    	    
+    	    ballot = Ballot.new(defendant, m.user, m.channel, type)
+    	    @ballots << ballot
+    	    
+    	    ballot.timeout_job = @@timer.schedule_in '5m' do
+    	        cancel(m)
+    	    end
+    	    
+    	    m.reply "Voting | #{defendant} | Vote started! Please vote on this ban with .vote yes|no"
 		rescue Exception => e
-			m.reply "VoteBan | Error: #{e}"
+			m.reply "Voting | Error: #{e}"
 		end
 	end
 	
--- lib/plugins/voteban/ballot.rb
+++ lib/plugins/voteban/ballot.rb
@@ -1,15 +1,28 @@
 class Ballot
     attr_accessor :timeout_job
-    attr_reader :yes, :no, :defendant, :starter, :voters, :channel
+    attr_reader :yes, :no, :defendant, :starter, :voters, :channel, :type
     
-    def initialize ( defendant, starter, channel )
+    def initialize ( defendant, starter, channel, type = :ban )
         @defendant = defendant
         @starter = starter
         @channel = channel
         @yes = 1
         @no = 0
         @voters = [@starter]
+        @type = valid_type type
     end 
+    
+    def valid_types
+        [:ban, :kick, :quiet]
+    end
+    
+    def valid_type? type
+        valid_types.include? type
+    end
+    
+    def valid_type type
+        valid_type?(type) ? type : :ban
+    end
     
     def vote ( vote, user )
         raise "Can't vote twice" if @voters.include? user
